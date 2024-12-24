2353de78bb56e61d00313cbb8c1360a4a7673cb5
mrgnschndr <mrgnschndr@gmail.com>
2024-12-23 18:48:31 -0600

    seedDB function first draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+
+  seedDB = async () => {
+    try {
+      let res = await axios.get('http://localhost:5002/users');
+      this.setState({
+        users: res.data
+      })
+      return res;
+    } catch(error) {
+      console.log(error.message)
+    }
+  }
+
+
+  
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+  )}
 }
 
 export default App;
